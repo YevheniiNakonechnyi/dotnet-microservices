c355b2347a92b080074555c7655c722f54102354
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2022-10-12 23:40:19 +0300

    Add projects and docker compose

---
+++ src/microservices/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private const string ConnectionString = "DatabaseSettings:ConnectionString";
+        private const string DatabaseName = "DatabaseSettings:DatabaseName";
+        private const string CollectionName = "DatabaseSettings:CollectionName";
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>(ConnectionString));
+            var database = client.GetDatabase(configuration.GetValue<string>(DatabaseName));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>(CollectionName));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
