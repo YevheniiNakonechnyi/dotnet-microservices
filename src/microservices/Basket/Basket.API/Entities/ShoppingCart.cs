11819cc415b7d0d020c0e8b0d813b3cb0b03aab2
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2022-10-20 21:49:15 +0300

    Create Basket.API

---
+++ src/microservices/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart() {
+            
+        }
+
+        public ShoppingCart(string userName) {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice => Items.Count > 0 
+                                         ? Items.Select(i => i.Price * i.Quantity).Sum()
+                                         : 0;
+    }
+}
