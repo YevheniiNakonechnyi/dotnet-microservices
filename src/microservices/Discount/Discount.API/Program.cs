9c792e81964e968164e2b4380ef90625c00fbe24
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2022-11-09 23:03:30 +0200

    Init database before running microservice

--- src/microservices/Discount/Discount.API/Program.cs
+++ src/microservices/Discount/Discount.API/Program.cs
@@ -1,19 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Discount.API.Extensions;
 
 namespace Discount.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().InitDatabase<Program>().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
