b2b2e454222fca34a507839f7f576a9dd57a1d82
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2022-11-17 22:30:45 +0200

    Create grpc client in Basket.API

--- src/microservices/Basket/Basket.API/Controllers/BasketController.cs
+++ src/microservices/Basket/Basket.API/Controllers/BasketController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Basket.API.Entities;
 using Basket.API.Repositories;
 using System.Net;
+using Basket.API.GrpcServices;
 
 namespace Basket.API.Controllers
 {
@@ -13,11 +14,13 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
         private readonly ILogger<BasketController> _logger;
 
-        public BasketController(IBasketRepository repository, ILogger<BasketController> logger)
+        public BasketController(IBasketRepository repository, ILogger<BasketController> logger, DiscountGrpcService discountGrpcService)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -31,6 +34,11 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            foreach (var item in basket.Items) {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
