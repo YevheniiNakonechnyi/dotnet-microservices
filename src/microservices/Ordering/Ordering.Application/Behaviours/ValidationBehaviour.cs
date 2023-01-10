71687794cb179e1d55b677032dc65eae8bc88eab
Евгений Наконечный <NakonechnyyEvgeniy@outlook.com>
2023-01-10 20:36:26 +0200

    Init layers

---
+++ src/microservices/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any()) 
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResults = await Task.WhenAll(_validators.Select(i => i.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(i => i.Errors).Where(i => i != null).ToList();
+
+                if (failures.Any())
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
